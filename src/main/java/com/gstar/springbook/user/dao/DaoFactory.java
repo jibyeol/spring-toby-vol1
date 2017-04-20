a54c3271bd242ada04c50e48a814627adb963f66
jibyeol <cjfrtt1234@naver.com>
2017-04-20 14:19:31 +0900

    1.5 스프링의 IoC
    1. 오브젝트 팩토리를 이용한 스프링 IoC
    2. 애플리케이션 컨텍스트의 동작방식
        장점)
          - 클라이언트는 구체적인 팩토리 클래스를 알 필요가 없다.
          - 애플리케이션 컨텍스트는 종합 IoC 서비스를 제공해준다.
          - 애플리케이션 컨텍스트는 빈을 검색하는 다양한 방법을 제공한다.
    3. 스프링 IoC의 용어 정리
        - bean
        - bean factory
        - application context
        - configuration metadata
        - container 또는 IoC container
        - spring framework

--- src/main/java/com/gstar/springbook/user/dao/DaoFactory.java
+++ src/main/java/com/gstar/springbook/user/dao/DaoFactory.java
@@ -1,22 +1,22 @@
 package com.gstar.springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration	// 스프링이 빈 팩토리를 위한 오브젝트 설정을 담당하는 
+				// 클래스라고 인식할 수 있도록 이 애노테이션 추가함
+				// 
+				// 애플리케이션 컨텍스트 또는 빈 팩도리가 사용할 설정정보라는 표시
 public class DaoFactory {
 
+	@Bean	// 오브젝트 생성을 담당하는 IoC용 메소드라는 표시
 	public UserDao userDao(){
 		UserDao userDao = new UserDao(connectionMaker());
 		return userDao;
 	}
-	
-	public AccountDao accountDao(){
-		AccountDao accountDao = new AccountDao(connectionMaker());
-		return accountDao;
-	}
-	
-	public MessageDao messageDao(){
-		MessageDao messageDao = new MessageDao(connectionMaker());
-		return messageDao;
-	}
-	
+
+	@Bean
 	public ConnectionMaker connectionMaker(){
 		return new DConnectionMaker();
 	}
--- src/main/java/com/gstar/springbook/user/dao/UserDaoTest.java
+++ src/main/java/com/gstar/springbook/user/dao/UserDaoTest.java
@@ -2,12 +2,16 @@ package com.gstar.springbook.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.gstar.springbook.user.domain.User;
 
 public class UserDaoTest {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new DaoFactory().userDao();
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("cjfrtt1234");
